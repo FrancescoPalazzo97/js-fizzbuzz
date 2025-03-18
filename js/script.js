6cca8139c43e14dda09f95a2f2beb213d219d602
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-18 14:19:58 +0100

    [UPD] Secondo blocco condizionale

--- js/script.js
+++ js/script.js
@@ -3,5 +3,7 @@ const word = `FizzBuzz`; // Definisco la variabile word e la inizializzo con il 
 for (let i = 1; i <= 100; i++) { // Creo un ciclo for che parte da 1 e termina a 100
     if (i % 3 === 0 && i % 5 === 0) { // Se il numero è divisibile sia per 3 sia per 5
         console.log(word); // Stampo la variabile word per intero
-    } // Chiudo il blocco if
+    } else if (i % 3 === 0) { // Se il numero è divisibile per 3
+        console.log(word.substring(0, 4)); // Stampo `Fizz`
+    }
 } // Chiudo il ciclo for
